da30cd758505ba7f0d5646ab8dd7735741fa9631
anjumzaki <anjumzaki8@gmail.com>
2018-11-15 03:37:49 +0500

    Finilized

--- src/App.js
+++ src/App.js
@@ -7,48 +7,54 @@ class App extends Component {
     super(props)
     var today = new Date(),
       date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    this.state = {
-      date:date,
-      amm:'',
-      desc:'',
-      type:'Income',
-       data:[],
-       ballance:0,
-       }
+     
+      this.state = {
+            date: date,
+            amm: '',
+            desc: '',
+            type: 'Income',
+            data: [],
+            ballance: 0,
+    }
     this.handleChange = this.handleChange.bind(this)
     this.handleCreateNote = this.handleCreateNote.bind(this)
   }
   handleCreateNote = () => {
-    var prev = this.state.ballance;
-     var amm = this.state.amm;
+    if(this.state.amm){
+    var prev = this.state.ballance; //Saving previous State for Ballnace summation
+    var amm = this.state.amm;       //savving current State from Sum
     let data = [...this.state.data];   //creating the copy
-
     //adding new data
     data.push({
       type: this.state.type,
-      date : this.state.date,
-      desc : this.state.desc,
-      amm : this.state.amm,
+      date: this.state.date,
+      desc: this.state.desc,
+      amm: this.state.amm,
     });
-
-    if(this.state.type==="Income"){
+    //Checking if this is Income then Add in THe balance
+    if (this.state.type === "Income") {
       this.setState({
-        ballance:prev- (-amm)
+        ballance: prev - (-amm)
       })
     }
-    if(this.state.type==="Expense"){
+    // Checking if this is Expense then Subtract from th Balance
+    if (this.state.type === "Expense") {
       this.setState({
-        ballance:prev-amm
+        ballance: prev - amm
       })
     }
     //updating the state value
     this.setState({ data });
-  };
-
+  }
+else{
+  alert('Please Enter Ammount First')
+}}
+  ;
+  //Dynamically Changing the States in Changes with Magic
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
- 
+
   render() {
     return (
       <div className="app">
@@ -56,7 +62,7 @@ class App extends Component {
           <span style={{ fontSize: 40, }}> Account Ballance </span>
         </div>
         <div className="select-container">
-          <input name="desc" placeholder="Description" type="text" value={this.state.desc} onChange={this.handleChange} className="inputst" />
+          <input  name="desc" placeholder="Description" type="text" value={this.state.desc} onChange={this.handleChange} className="inputst" />
           <input  name="amm" placeholder="Ammount" type="number" value={this.state.amm} onChange={this.handleChange} className="inputst" />
           <div>
 
@@ -65,68 +71,63 @@ class App extends Component {
               <option value="Income">Income</option>
               <option value="Expense">Expense</option>
             </select>
-            <button onClick={this.handleCreateNote}>ADD</button>
+            <button onClick={this.handleCreateNote} className="button">ADD</button>
           </div>
         </div>
         <table className="frtable" >
           <thead>
             <tr>
-            <td>Income</td>
-            <td>Expense</td>
+              <td>Income</td>
+              <td>Expense</td>
             </tr>
           </thead>
           <tbody>
             <tr>
-           <td>
-            { 
-            this.state.data.map((data) =>
-            {
-            if(data.type=='Income'){
-                 return <li>
-                {data.desc}-
-                {data.amm} $-
-                {data.date}
-                
-              </li>
-              
-            }
-          }
-            )
-            } 
-            </td>
-             <td>
-             {
-            this.state.data.map((data) =>
-            {
-            if(data.type=='Expense'){
-             
-              return               <li>
-                 {data.desc}-
-                {data.amm} $-
-                {data.date}
-              </li>
-              
-            }
-          }
-            )
-            } 
-             </td>
+              <td>
+                {
+                  this.state.data.map((data) => {
+                    if (data.type == 'Income') {
+                      return <li>
+                        {data.desc}-
+                        {data.amm} $-
+                        {data.date}
+                      </li>
+                    }
+                  }
+                 )
+                }
+              </td>
+              <td>
+                {
+                  this.state.data.map((data) => {
+                    if (data.type == 'Expense') {
+                      return <li>
+                        {data.desc}  - 
+                        {data.amm}  $- 
+                        {data.date}
+                      </li>
+
+                    }
+                  }
+                  )
+                }
+              </td>
             </tr>
-            
-             
-          
-            
+
+
+
+
           </tbody>
-          
+
         </table>
         <div className="forbal">
-        <hr></hr>
-          <span>
+          <hr></hr>
+          <span style={{color:'#ff1769'}}>
             Ballance :
             {this.state.ballance} $
           </span>
         </div>
-       
+
       </div>
     )
   }
