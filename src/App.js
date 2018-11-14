f3a56cad72dbcd902568f1fbf1df99325b0ea583
anjumzaki <anjumzaki8@gmail.com>
2018-11-14 13:03:30 -0800

    before balance

--- src/App.js
+++ src/App.js
@@ -3,46 +3,110 @@ import React, { Component } from 'react';
 import './App.css'
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     var today = new Date(),
-            date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-
-
-    this.state = {currency:'Income' , date:date}
-    this.onCurrencySelect = this.onCurrencySelect.bind(this)
+      date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    this.state = {
+      date:date,
+      amm:'',
+      desc:'',
+      type:'Income',
+       data:[]
+       }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleCreateNote = this.handleCreateNote.bind(this)
   }
+  handleCreateNote = () => {
+    let data = [...this.state.data];   //creating the copy
 
-  onCurrencySelect (e) {
-    this.setCurrency(e.target.value)
+    //adding new data
+    data.push({
+      type: this.state.type,
+      date : this.state.date,
+      desc : this.state.desc,
+      amm : this.state.amm,
+    });
+    //updating the state value
+    this.setState({ data });
+    console.log(this.state.data)
+  };
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
-setCurrency(inc){
-  this.setState({currency:inc})
-}
+  
   render() {
-      return (
-        <div className="app">
-        <div style={{margin:'auto'}}>
-        <span style={{fontSize: 40, fontFamily: 'Bungee'}}> Account Ballance </span>
+    return (
+      <div className="app">
+        <div style={{ margin: 'auto' }}>
+          <span style={{ fontSize: 40, }}> Account Ballance </span>
         </div>
-          <div className="select-container">
-          <input placeholder="Description" type="text"  className="inputst" /> 
-          <input placeholder="Ammount" type="number" className="inputst"/> 
+        <div className="select-container">
+          <input name="desc" placeholder="Description" type="text" value={this.state.desc} onChange={this.handleChange} className="inputst" />
+          <input  name="amm" placeholder="Ammount" type="number" value={this.state.amm} onChange={this.handleChange} className="inputst" />
           <div>
-     
-            <span style={{fontSize: 18, fontFamily: 'Bungee'}}> Transaction Type </span>
-            <select value={this.state.currency} onChange={this.onCurrencySelect}>
-            
-            <option  value="Income">Income</option>
-            <option value="Expense">Expense</option>
-            </select>
-            </div>
-          </div>
 
+            <span style={{ fontSize: 18, }}> Transaction Type </span>
+            <select name="type" value={this.state.type} onChange={this.handleChange}>
+              <option value="Income">Income</option>
+              <option value="Expense">Expense</option>
+            </select>
+            <button onClick={this.handleCreateNote}>ADD</button>
+          </div>
         </div>
-      )
-    }
+        <table className="frtable" >
+          <thead>
+            <tr>
+            <td>Income</td>
+            <td>Expense</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+           <td>
+            {
+            this.state.data.map((data) =>
+            {
+            if(data.type=='Income'){
+              return <li>
+                {data.desc}-
+                {data.amm} $-
+                {data.date}
+                
+              </li>
+              
+            }
+          }
+            )
+            } 
+            </td>
+             <td>
+             {
+            this.state.data.map((data) =>
+            {
+            if(data.type=='Expense'){
+              return               <li>
+                 {data.desc}-
+                {data.amm} $-
+                {data.date}
+              </li>
+              
+            }
+          }
+            )
+            } 
+             </td>
+            </tr>
+            
+             
+          
+            
+          </tbody>
+        </table>
+      </div>
+    )
   }
+}
 
 
 export default App;
